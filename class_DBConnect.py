695b8598a083538d269078e52929bc214502ddfe
SudatiSimone <39488019+SudatiSimone@users.noreply.github.com>
2020-09-22 16:04:13 +0100

    update

--- class_DBConnect.py
+++ class_DBConnect.py
@@ -1,32 +1,34 @@
 import sqlite3
 
+
 class DBConnect:
 
     def __init__(self):
-        self._db = sqlite3.connect("information.db")  # extension for sqlite database
+        self._db = sqlite3.connect(
+            "Account_database.db")  # extension for sqlite database
         self._db.row_factory = sqlite3.Row
         self._db.execute("create table if not exists Admin(ID integer primary "
-                   "key autoincrement, Name text,Age int)")
+                         "key autoincrement, Username text,NumericPassword int)")
         self._db.commit()
 
-    def Add(self, Name, Age):
-        self._db.execute("insert into Admin(Name,Age) values (?,?)", (Name, Age))
+    def Add(self, Username, NumericPassword):
+        self._db.execute("insert into Admin(Username, NumericPassword) values (?,?)""",
+                         (Username, NumericPassword))
         self._db.commit()
         print("Record is added")
 
     def ListAdmins(self):
         cursor = self._db.execute("select * from Admin")
         for row in cursor:
-            print("ID {}: , Name: {}, Age: {}".format(row["ID"], row["Name"],
-                                                      row["Age"]))
+            print("ID {}: , Username: {}, Password: {}".format(row["ID"], row["Username"],
+                                                      row["NumericPassword"]))
 
-    def DeleteRecord(self,ID):
+    def DeleteRecord(self, ID):
         self._db.execute("delete from admin where ID='{}'".format(ID))
         self._db.commit()
         print("Record is deleted")
 
-    def UpdateRecord(self, ID, Age):
-        self._db.execute("update  Admin set Age=? where ID=?", (Age, ID))
+    def UpdateRecord(self, ID, NumericPassword):
+        self._db.execute("update  Admin set NumericPassword=? where ID=?", (NumericPassword, ID))
         self._db.commit()
         print("Record is added")
-
--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 from class_DBConnect import DBConnect
+
+
 def main():
-    print("Benvenuto")
+    print("\nBenvenuto nella gestione degli account")
     dbConnect = DBConnect()
     while 1:
         IndexOp = int(input(
-            "\n\n=========Select Operation,\n1-add:\n2-list Admins:\n3-delete by name:\n4-update\n0- exit:\n===============\n\n"))
-        if (IndexOp == 0):
+            "\n=============\nSelect Operation,\n1-add account\n2-list "
+            "account\n3-delete account "
+            "by ID\n4-update account\n0- exit\n=============\nChoice: "))
+        if IndexOp == 0:
             break;
-        elif (IndexOp == 1):
-            Name = input("Enter name: ")
-            Age = int(input("Enter age: "))
-            dbConnect.Add(Name, Age)
-        elif (IndexOp == 2):
+        elif IndexOp == 1:
+            Username = input("Enter Username: ")
+            NumericPassword = int(input("Enter numeric password: "))
+            dbConnect.Add(Username, NumericPassword)
+        elif IndexOp == 2:
             dbConnect.ListAdmins()
-        elif (IndexOp == 3):
+        elif IndexOp == 3:
             ID = input("ID to delete: ")
             dbConnect.DeleteRecord(ID)
-        elif (IndexOp == 4):
+        elif IndexOp == 4:
             ID = input("Enter ID: ")
-            Age = int(input("Enter age: "))
-            dbConnect.UpdateRecord(ID, Age)
+            NumericPassword = int(input("Enter numeric Password: "))
+            dbConnect.UpdateRecord(ID, NumericPassword)
 
 
 if __name__ == '__main__': main()
